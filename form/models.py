dfe2496da755a97c9426f562b0dba838d7a61f94
Lucas Varela <hakuro.lucas@gmail.com>
2017-08-03 16:45:12 -0300

    adds option to models

--- form/models.py
+++ form/models.py
@@ -11,6 +11,7 @@ class Client(models.Model):
     cep = models.CharField(max_length=10, blank=False)
     iptu_price = models.DecimalField(max_digits=8, decimal_places=2)
     neighbourhood = models.CharField(max_length=200, blank=True)
+    optin = models.BooleanField(default=True, blank=False)
 
     def __unicode__(self):
         return self.name
